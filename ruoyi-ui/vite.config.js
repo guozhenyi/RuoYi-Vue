2d1544ab7cfc09c407123038aa3856b8affd13bc
gzy <gzyonline@hotmail.com>
2024-12-03 15:33:52 +0800

    引入vite

---
+++ ruoyi-ui/vite.config.js
@@ -0,0 +1,19 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+import vueJsx from '@vitejs/plugin-vue2-jsx'
+import removeConsole from 'vite-plugin-remove-console'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    removeConsole()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+})
